c03a53bfacdfe3017faeadd291a5cf1a908ba81a
aitm12 <aitm814@gmail.com>
2018-04-22 15:30:13 -0700

    fixed iptables.sh

--- home/iptables_folder/iptables.sh
+++ home/iptables_folder/iptables.sh
@@ -1,7 +1,10 @@
+#! /bin/bash
+
 ##iptables
 
 # clear tables
-iptables -F && iptables -X
+iptables -F
+iptables -X
 iptables -Z
 
 # default policy chain
@@ -10,12 +13,12 @@ iptables -P FORWARD DROP
 iptables -P OUTPUT DROP
 
 # loop back
-iptables -i lo -j ACCEPT
-iptables -o lo -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
 
 # outbound dns
-iptables -A OUTPUT -o eno1 -p udp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i eno1 -p udp --dport -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eno1 -p udp --dport 53 -j ACCEPT
+iptables -A INPUT -i eno1 -p udp --sport 53 -j ACCEPT
 
 # outbound ping
 iptables -A OUTPUT -o eno1 -p icmp --icmp-type echo-request -j ACCEPT
@@ -27,17 +30,17 @@ iptables -A INPUT  -i eno1 -p udp --sport 123 -m state --state ESTABLISHED -j AC
 
 # outbound rsync
 iptables -A OUTPUT -o eno1 -p tcp -d rsync.gentoo.org --dport 873 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables pA INPUT -i eno1 -p tcp -d rsync.gentoo.org --sport 873 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -i eno1 -p tcp --sport 873 -m state --state ESTABLISHED -j ACCEPT
 
 # outbound portage mirrors
 # inbound rule is at http
 iptables -A OUTPUT -o eno1 -p tcp -d gentoo.mirrors.easynew.com --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eno1 -p tcp -d www.gtlib.gatech.edu --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -o OUTPUT -o eno1 -p tcp -d gentoo.cs.utah.edu --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eno1 -p tcp -d gentoo.cs.utah.edu --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 
 # outbound HTTP
 iptables -A OUTPUT -o eno1 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i eno1 -p tcp --sport 80 -m limit --limit 25/minute --limit-burst 100 state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -i eno1 -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
 
 # outbound HTTPS
 iptables -A OUTPUT -o eno1 -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
@@ -58,7 +61,7 @@ iptables -A OUTPUT -o eno1 -p tcp --dport 143 -m state --state NEW,ESTABLISHED -
 iptables -A INPUT -i eno1 -p tcp --sport 143 -m state --state ESTABLISHED -j ACCEPT
 
 iptables -A OUTPUT -o eno1 -p tcp --dport 993 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INOUT -i eno1 -p tcp --sport 993 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -i eno1 -p tcp --sport 993 -m state --state ESTABLISHED -j ACCEPT
 
 # pop3 pop3s
 iptables -A OUTPUT -o eno1 -p tcp --dport 110 -m state --state NEW,ESTABLISHED -j ACCEPT
@@ -73,7 +76,15 @@ iptables -A INPUT -i eno1 -p tcp --sport 995 -m state --state ESTABLISHED -j ACC
 iptables -A OUTPUT -o eno1 -p tcp --dport 9150 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i eno1 -p tcp --sport 9150 -m state --state ESTABLISHED -j ACCEPT
 
+# Prevent DDOS
+iptables -A INPUT -p tcp --dport 80 -m limit --limit 25/minute --limit-burst 100 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -m limit --limit 25/minute --limit-burst 100 -j ACCEPT
+
 # Dropping all other
-iptables -A INPUT DROP
-iptables -A FORWARD DROP
-iptables -A OUTPUT DROP
+iptables -A INPUT -j DROP
+iptables -A FORWARD -j DROP
+iptables -A OUTPUT -j DROP
+
+# list rules
+iptables -L
+iptables -S
